2d189333a1749ca06290a771d841cf357ed67a35
sernav <sergio.navarrogil@gmail.com>
2013-03-08 18:10:40 +0100

    Change-Id: Ibad2a224c8239250aa84850208e858b573e34d59

---
+++ WorkSRILanda/src/es/uclm/sri/persistencia/ConnectionFactory.java
@@ -0,0 +1,27 @@
+package es.uclm.sri.persistencia;
+
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class ConnectionFactory {
+	
+	private static SqlSessionFactory sqlMapper;
+    private static Reader reader; 
+
+    static{
+        try{
+            reader = Resources.getResourceAsReader("configurationPostgre.xml");
+            sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSessionFactory getSession(){
+        return sqlMapper;
+    }
+
+}
