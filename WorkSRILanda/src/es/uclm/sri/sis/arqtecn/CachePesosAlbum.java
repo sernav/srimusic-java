58521e62a575a16ad235ccd094ca9ab5c29f739a
sernav <sergio.navarrogil@gmail.com>
2013-03-16 18:30:07 +0100

    Manejo caché de albums
    Change-Id: Iaa4f548a6dcf15a23679d23f69307f713aa2dbb9
    Signed-off-by: sernav <sergio.navarrogil@gmail.com>

---
+++ WorkSRILanda/src/es/uclm/sri/sis/arqtecn/CachePesosAlbum.java
@@ -0,0 +1,57 @@
+package es.uclm.sri.sis.arqtecn;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.Element;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import es.uclm.sri.cache.EhcacheFactory;
+import es.uclm.sri.persistencia.ConnectionFactory;
+import es.uclm.sri.persistencia.postgre.dao.VpesosalbumMapper;
+import es.uclm.sri.persistencia.postgre.dao.model.Vpesosalbum;
+
+public class CachePesosAlbum extends AbstractCacheoPesos {
+
+	private Cache cachePesosAlbum = null;
+	SqlSessionFactory sqlMapper = ConnectionFactory.getSession();
+	SqlSession session = null;
+	Element eVPesosAlbum = null;
+	
+	public void getElementosTablaById(Integer id) {
+		try{
+			sqlMapper.openSession();
+			VpesosalbumMapper mapper = session.getMapper(VpesosalbumMapper.class);
+			Vpesosalbum vPesosAlbum = mapper.selectByPrimaryKey(id);
+			eVPesosAlbum = new Element(vPesosAlbum.getID_VPESOSA(), vPesosAlbum);
+		} finally {
+			session.close();
+		}	
+		
+	}
+
+	public void loadCache() {
+		EhcacheFactory.setCache(KConstantes.Cache.cacheVPesosAlbum);
+		cachePesosAlbum = EhcacheFactory.getCache();
+		
+	}
+
+	public int loadElementosDCache() {
+		try{
+			cachePesosAlbum.put(eVPesosAlbum);
+			return 1;
+		} catch (Exception e){
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	public Cache getCacheCompleta() {
+		return cachePesosAlbum;
+	}
+
+	public Object getElementoByKey(String key) {
+		return cachePesosAlbum.get(key);
+	}
+
+}
