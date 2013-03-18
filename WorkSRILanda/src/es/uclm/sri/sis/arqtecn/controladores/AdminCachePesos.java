0181b96b79699c46804bea1d89145e57fb4f042c
sernav <sergio.navarrogil@gmail.com>
2013-03-18 10:06:44 +0100

    Manejo de cachés

--- WorkSRILanda/src/es/uclm/sri/sis/arqtecn/controladores/AdminCachePesos.java
+++ WorkSRILanda/src/es/uclm/sri/sis/arqtecn/controladores/AdminCachePesos.java
@@ -1,7 +1,22 @@
 package es.uclm.sri.sis.arqtecn.controladores;
 
+import java.sql.Connection;
+
+import org.postgresql.core.v2.ConnectionFactoryImpl;
+
 public class AdminCachePesos {
 	
+	public AdminCachePesos() {
+		
+	}
 	
+	public static AdminCachePesos getInstance(String idConnection) {
+		return null;
+	}
+	
+	public static AdminCachePesos getInstance() {
+		Connection connection = (Connection) new ConnectionFactoryImpl();
+		return null;
+	}
 
 }
