26e24a24a0dfa2be47b2fb23a24db246b7898938
sernav <sergio.navarrogil@gmail.com>
2013-03-24 22:21:09 +0100

    Constantes de tablas de bases de datos y reflexión.
    Change-Id: If36237e9d1228102ab10d9661b315aaac958f86a
    Signed-off-by: sernav <sergio.navarrogil@gmail.com>

--- WorkSRILanda/src/es/uclm/sri/sis/arqtecn/KConstantes.java
+++ WorkSRILanda/src/es/uclm/sri/sis/arqtecn/KConstantes.java
@@ -9,8 +9,20 @@ public class KConstantes {
 		
 	}
 	
+	public class Tablas {
+		public static final String albums = "ALBUMSAPP";
+		public static final String users = "USERSAPP";
+		public static final String historico = "HISTUSER";
+		public static final String pesosUser = "VPESOSUSER";
+		public static final String pesosAlbum = "VPESOSALBUM";
+	}
+	
 	public class Reflexion {
 		public static final String rootElement = "CRAWLER";
+		public static final String pathXmlClasesReflexion = "/es/uclm/sri/recursos/clasesReflexion.xml";
+		public static final String attrbIsEnable = "isEnabled";
+		public static final String attrbClass = "class";
 	}
+
 
 }
