cdd08b37fef3c741007f8828863df49a414dd670
sernav <sergio.navarrogil@gmail.com>
2014-06-19 20:43:16 +0200

    Launcher

--- WorkSRILanda/src/main/java/es/uclm/sri/sis/SriLandaTest.java
+++ WorkSRILanda/src/main/java/es/uclm/sri/sis/SriLandaTest.java
@@ -1,7 +1,5 @@
 package main.java.es.uclm.sri.sis;
 
-import java.io.File;
-
 import main.java.es.uclm.sri.lastfm.PlaybackDUsuario;
 import main.java.es.uclm.sri.sis.fabricas.FabricaDRecomendaciones;
 
@@ -11,13 +9,9 @@ public class SriLandaTest {
      * @param args
      */
     public static void main(String[] args) {
-        
-//        ClustererSri analisisClustering = ClustererSri.getInstance();
 
         PlaybackDUsuario playback = new PlaybackDUsuario("djAguadilla");
         playback.run();
-        
-        File file = new File("es/uclm/sri/recursos/xml/sqlMapConfig.xml");
         
         FabricaDRecomendaciones mainFactory = new FabricaDRecomendaciones(playback);
         mainFactory.run();
